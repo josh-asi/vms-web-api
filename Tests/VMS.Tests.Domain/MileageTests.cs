88f3427fd44c191c630537e0f4aecc3b1e3d46d4
Unknown <josh.asi@hotmail.co.nz>
2020-07-14 18:37:46 +1200

    Created a kilometre object to be used for mileage

---
+++ Tests/VMS.Tests.Domain/MileageTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using VMS.Domain.Aggregates.VehicleAggregate;
+using Xunit;
+
+namespace VMS.Tests.Domain
+{
+    public class MileageTests
+    {
+        [Fact]
+        public void Mileage_Should_Only_Be_Increased()
+        {
+            var mileage = new Kilometres(100.0f);
+            var newMileage = new Kilometres(120.0f);
+
+            mileage.UpdateMileage(newMileage);
+            Assert.Equal(newMileage, mileage);
+
+            Assert.Throws<InvalidOperationException>(() => mileage.UpdateMileage(90.0f));
+        }
+    }
+}
