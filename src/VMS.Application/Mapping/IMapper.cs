d928c187e585eb35bc1d51dcf93fa86db214a2ee
Unknown <josh.asi@hotmail.co.nz>
2020-07-15 10:38:06 +1200

    Created a custom mapper to map domain model to dto

---
+++ src/VMS.Application/Mapping/IMapper.cs
@@ -0,0 +1,7 @@
+﻿namespace VMS.Application.Mapping
+{
+    public interface IMapper<in Tsource, Tout> where Tsource : class
+    {
+        public Tout Map(Tsource source);
+    }
+}
---
+++ src/VMS.Infrastructure/Mapping/VehicleMapper.cs
@@ -0,0 +1,20 @@
+﻿using VMS.Application.DTOs.Vehicle;
+using VMS.Application.Mapping;
+using VMS.Domain.Aggregates.VehicleAggregate;
+
+namespace VMS.Infrastructure.Mapping
+{
+    public class VehicleMapper : IMapper<Vehicle, VehicleDTO>
+    {
+        public VehicleDTO Map(Vehicle source)
+        {
+            return new VehicleDTO
+            (
+                source.Id,
+                source.Speed,
+                source.Mileage,
+                source.VehicleType.ToFriendlyString()
+            );
+        }
+    }
+}
