3bf0acb4fea8c29fcd6b316626042988e44faa7f
Unknown <josh.asi@hotmail.co.nz>
2020-07-14 18:17:14 +1200

    Created initial domain objects

---
+++ src/VMS.Domain/Aggregates/VehicleAggregate/Location.cs
@@ -0,0 +1,16 @@
+﻿using VMS.Domain.SeedWork;
+
+namespace VMS.Domain.Aggregates.VehicleAggregate
+{
+    public class Location : Entity
+    {
+        public Latitude Latitude { get; private set; }
+        public Longitude Longitude { get; private set; }
+
+        public Location(Latitude latitude, Longitude longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+    }
+}
---
+++ src/VMS.Domain/Aggregates/VehicleAggregate/VehicleType.cs
@@ -0,0 +1,16 @@
+﻿namespace VMS.Domain.Aggregates.VehicleAggregate
+{
+    public enum VehicleType
+    {
+        Truck = 0,
+        Bus = 1
+    }
+
+    public static class VehicleTypeExtensions
+    {
+        public static VehicleType ToVehicleType(this int type)
+        {
+            return (VehicleType)type;
+        }
+    }
+}
