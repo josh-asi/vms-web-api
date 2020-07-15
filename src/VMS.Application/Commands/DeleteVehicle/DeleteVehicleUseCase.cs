6db26b17fa21b7a49dfc6e74d0d0078410d69b43
Unknown <josh.asi@hotmail.co.nz>
2020-07-15 12:35:52 +1200

    Implemented delete vehicle use case

---
+++ src/VMS.Application/Commands/DeleteVehicle/DeleteVehicleUseCase.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using VMS.Application.Exceptions;
+using VMS.Application.UnitOfWork;
+
+namespace VMS.Application.UseCases.DeleteVehicle
+{
+    public class DeleteVehicleUseCase : IDeleteVehicleUseCase
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public DeleteVehicleUseCase(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task Execute(int vehicleId)
+        {
+            try
+            {
+                await unitOfWork.VehicleRepository.DeleteAsync(vehicleId);
+            }
+            catch
+            {
+                await unitOfWork.RollBackAsync();
+                throw new ApplicationException("Failed to delete vehicle.");
+            }
+
+            await unitOfWork.CommitAsync();
+        }
+    }
+}
---
+++ src/VMS.Application/Exceptions/ApplicationException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace VMS.Application.Exceptions
+{
+    public class ApplicationException : Exception
+    {
+        public ApplicationException()
+        {
+        }
+
+        public ApplicationException(string message) : base(message)
+        {
+        }
+
+        public ApplicationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ApplicationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
