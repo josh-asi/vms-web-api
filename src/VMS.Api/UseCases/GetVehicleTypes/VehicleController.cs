1ba3afbeb64cfc225141a287d98922ac556a9c58
Unknown <josh.asi@hotmail.co.nz>
2020-07-15 18:57:54 +1200

    Created an endpoint for querying vehicle types

---
+++ src/VMS.Api/UseCases/GetVehicleTypes/VehicleController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using VMS.Application.Queries;
+
+namespace VMS.Api.UseCases.GetVehicleTypes
+{
+    [Route("api/[controller]/Types")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+        private readonly IVehicleQueries vehicleQueries;
+
+        public VehicleController(IVehicleQueries vehicleQueries)
+        {
+            this.vehicleQueries = vehicleQueries;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetVehicleTypesAsync()
+        {
+            try
+            {
+                return Ok(await vehicleQueries.GetAllVehicleTypesAsync());
+            }
+            catch
+            {
+                return BadRequest("Failed to get vehicle types");
+            }
+        }
+    }
+}
